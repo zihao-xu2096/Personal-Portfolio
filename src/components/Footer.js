3d94a6cc28b1980a76eb4125ea9ffb522a17dad9
Harry Xu <harryxcz@gmail.com>
2023-02-02 14:33:57 -0500

    Remove MailChimpForm

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { MailchimpForm } from "./MailchimpForm";
+// import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/linkedin.svg";
 import navIcon2 from "../assets/img/github.svg";
@@ -10,7 +10,6 @@ export const Footer = () => {
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          <MailchimpForm />
           <Col size={12} sm={6}>
             <img src={logo} alt="Logo" />
           </Col>
